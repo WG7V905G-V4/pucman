7088c759923371f4c40f02846ab9fa842cb02481
Savva <sawwatheowl@gmail.com>
2026-01-15 21:09:30 +0200

    нн

--- Ghostclass.py
+++ Ghostclass.py
@@ -1,6 +1,20 @@
 import arcade
 import random
-class Ghost(arcade.Sprite):
+
+class Character(arcade.Sprite):
+    def __init__(self, center_x, center_y, speed):
+        TILE_SIZE = 32
+        super().__init__()
+        radius = TILE_SIZE // 2 - 2
+        self.width = 30
+        self.height = 30
+        self.center_x = center_x
+        self.center_y = center_y
+        self.speed = speed
+        self.change_x = 0
+        self.change_y = 0
+
+class Ghost(Character):
     def __init__(self, center_x, center_y, speed = 0):
         super().__init__(center_x, center_y, speed)
         self.time_to_change_direction = 0
