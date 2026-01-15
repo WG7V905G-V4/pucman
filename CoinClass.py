4935fe19bd17a7fc66a0368cb30b5a82da9702e6
Vladostata15 <vladbondarenko944@gmail.com>
2026-01-15 22:45:32 +0200

    coin change

--- CoinClass.py
+++ CoinClass.py
@@ -7,7 +7,7 @@ class Coin(arcade.Sprite):
         self.center_x = x
         self.center_y = y
         self.size = TILE_SIZE//2
-        texture = arcade.make_circle_texture(self.size*2, arcade.color.YELLOW)
+        texture = arcade.make_circle_texture(self.size, arcade.color.WHITE)
         self.texture = texture
 
 
